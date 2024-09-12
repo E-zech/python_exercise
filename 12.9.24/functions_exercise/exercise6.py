20cd049c8f34fe2befcd391e18ed54072d65fb9e
E-zech <135145193+E-zech@users.noreply.github.com>
2024-09-12 10:08:23 +0300

    added

--- 12.9.24/functions_exercise/exercise6.py
+++ 12.9.24/functions_exercise/exercise6.py
@@ -1,16 +1,39 @@
 # Create a recursive function
-# Write a program to create a recursive function to calculate the sum of numbers from 0 to 10.
+# Write a program to create a recursive function to calculate the sum of numbers from 0 to 5.
 # A recursive function is a function that calls itself again and again.
 
-num = 0
 
 def calc(num):
-    print(num)
-    num += 1
 
-    if num <= 10:
-        calc(num)
+    if num:
+        return num + calc(num - 1)
+
     else:
-        return (print('yep'))
+        return 0
 
-calc(num)
+print(calc(5)) # 15
+
+# 1 round:
+# calc(5):
+#     return 5 + calc(5 - 1)   |   # return 15
+
+# 2 round:
+# calc(4):
+#     return 4 + calc(4 - 1)   |   # return 10
+
+# 3 round:
+# calc(3):
+#     return 3 + calc(3 - 1)   |   # return 6
+
+# 4 round:
+# calc(2):
+#     return 2 + calc(2 - 1)   |   # return 3
+
+# 5 round:
+# calc(1):
+#     return 1 + calc(1 - 1)   |   # return 1
+
+# 6 round:
+# calc(0):
+#     return 0 + calc(0 - 1)   |   # return 0 
+
