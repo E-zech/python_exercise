783e6dcd808fe830bd29b58161566bc3c989b05a
E-zech <135145193+E-zech@users.noreply.github.com>
2024-09-19 08:38:52 +0300

    added

--- missions/mission2.py
+++ missions/mission2.py
@@ -1,7 +1,6 @@
 # extract from 'mission1.csv' the conent
 # change the date format to: 01/01/2024 :'%d/%m/%Y %H:%M:%S'
 # exract only the 16/09/2024 entries 
-# save in new files while each file contain the same log level entreies
 
 from datetime import datetime as dt
 import re
@@ -14,8 +13,8 @@ print(specipec_date)
 
 
 df = pd.read_csv(file_path)
-df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d/%m/%Y %H:%M:%S')
-# print(df['timestamp'])
+df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S')
+print(df['timestamp'])
 
 
 df_spec_time = [df['timestamp'] < specipec_date]
